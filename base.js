5fce03dd61c99d6789d41f5817536cdc51da21f7
Heark <adtune6509@student.waynecc.edu>
2017-04-15 03:13:04 -0400

    Update base.js

--- base.js
+++ base.js
@@ -1,8 +1,8 @@
-/*
-Equations of motion at constant acceleration
-average speed in m/s = final velocity + initial velocity / 2
-*/
-
-function EqMotionConstantAcc() {
-
+function EqMotionConstantAcc(a, b, c, d, e) {
+    var f;
+    return null === a ? (a = b + c, a /= 2, !0 === d && (a = Math.round(a)), f = !0 === e ? "Average speed of the object at constant acceleration is: " + a + "m/s" : a) : null === b || (a = b + c, a /= 2, !0 === d && (a = Math.round(a))), f
 }
+var P = {
+    E: Math.E,
+    Pi: Math.PI
+};
